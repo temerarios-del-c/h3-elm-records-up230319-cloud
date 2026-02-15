bfac1948557ddb4fd52147767dd97f1972d10994
Natalia Gonzalez <up230319@alumnos.upa.edu.mxgit config --global user.name up230319-cloudgit pushexit:exitr{gtk+3{rgg{rr{g{{rg{r{gr>
2026-02-15 14:27:01 -0600

    formato

--- src/Helper.elm
+++ src/Helper.elm
@@ -2,41 +2,66 @@ module Helper exposing (..)
 
 import Html exposing (Html, div, li, text, ul)
 
+
+
 -- PARTE 1 - FUNCIONES BÁSICAS
+
+
 add2 : Int -> Int -> Int
 add2 x y =
     x + y
+
+
 add3 : Int -> Int -> Int -> Int
 add3 x y z =
     x + y + z
+
+
 calc : Int -> Int -> (Int -> Int -> Int) -> Int
 calc x y operation =
     operation x y
+
+
+
 -- PARTE 2 - RECORD ProgrammingLanguage
+
+
 type alias ProgrammingLanguage =
     { name : String
     , releaseYear : Int
     , currentVersion : String
     }
+
+
 languages : List ProgrammingLanguage
 languages =
     [ { name = "elm", releaseYear = 2012, currentVersion = "0.19.1" }
     , { name = "javascript", releaseYear = 1995, currentVersion = "ECMAScript 2025" }
     ]
+
+
 languageNames : List ProgrammingLanguage -> List String
 languageNames langs =
     List.map .name langs
 
+
+
 -- PARTE 3 - RECORD User
+
+
 type alias User =
     { name : String
     , uType : String
     }
+
+
 users : List User
 users =
     [ { name = "Roberto", uType = "Student" }
     , { name = "Mitsiu", uType = "Professor" }
     ]
+
+
 onlyStudents : List User -> List String
 onlyStudents userList =
     List.map
@@ -50,6 +75,7 @@ onlyStudents userList =
         )
         userList
 
+
 type alias Videogame =
     { title : String
     , releaseYear : Int
@@ -57,6 +83,8 @@ type alias Videogame =
     , downloads : Int
     , genres : List String
     }
+
+
 videogames : List Videogame
 videogames =
     [ { title = "Control"
@@ -72,9 +100,12 @@ videogames =
       , genres = [ "Action", "Adventure" ]
       }
     ]
+
+
 getVideogameGenres : List Videogame -> List (List String)
 getVideogameGenres games =
     List.map .genres games
+
 
 type alias Computer =
     { ram : String
@@ -82,6 +113,8 @@ type alias Computer =
     , brand : String
     , screenSize : String
     }
+
+
 myLaptop : Computer
 myLaptop =
     { ram = "16GB"
@@ -89,6 +122,8 @@ myLaptop =
     , brand = "Apple"
     , screenSize = "14"
     }
+
+
 main : Html msg
 main =
     div []
@@ -101,4 +136,3 @@ main =
                 ]
             ]
         ]
-
