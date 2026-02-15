6b1097f654b1f221bd2f0a2d30630b052a1f7b25
Natalia Gonzalez <up230319@alumnos.upa.edu.mxgit config --global user.name up230319-cloudgit pushexit:exitr{gtk+3{rgg{rr{g{{rg{r{gr>
2026-02-15 14:25:09 -0600

    correccion_3

--- src/Helper.elm
+++ src/Helper.elm
@@ -1,49 +1,42 @@
 module Helper exposing (..)
 
-import Html exposing (Html, text)
-import String
+import Html exposing (Html, div, li, text, ul)
 
-
-
--- EJERCICIO 1
-
-
+-- PARTE 1 - FUNCIONES BÁSICAS
+add2 : Int -> Int -> Int
+add2 x y =
+    x + y
+add3 : Int -> Int -> Int -> Int
+add3 x y z =
+    x + y + z
+calc : Int -> Int -> (Int -> Int -> Int) -> Int
+calc x y operation =
+    operation x y
+-- PARTE 2 - RECORD ProgrammingLanguage
 type alias ProgrammingLanguage =
     { name : String
     , releaseYear : Int
     , currentVersion : String
     }
-
-
 languages : List ProgrammingLanguage
 languages =
     [ { name = "elm", releaseYear = 2012, currentVersion = "0.19.1" }
     , { name = "javascript", releaseYear = 1995, currentVersion = "ECMAScript 2025" }
     ]
-
-
 languageNames : List ProgrammingLanguage -> List String
 languageNames langs =
     List.map .name langs
 
-
-
--- EJERCICIO 2
-
-
+-- PARTE 3 - RECORD User
 type alias User =
     { name : String
     , uType : String
     }
-
-
 users : List User
 users =
     [ { name = "Roberto", uType = "Student" }
     , { name = "Mitsiu", uType = "Professor" }
     ]
-
-
 onlyStudents : List User -> List String
 onlyStudents userList =
     List.map
@@ -57,11 +50,6 @@ onlyStudents userList =
         )
         userList
 
-
-
--- EJERCICIO 3
-
-
 type alias Videogame =
     { title : String
     , releaseYear : Int
@@ -69,8 +57,6 @@ type alias Videogame =
     , downloads : Int
     , genres : List String
     }
-
-
 videogames : List Videogame
 videogames =
     [ { title = "Control"
@@ -86,13 +72,33 @@ videogames =
       , genres = [ "Action", "Adventure" ]
       }
     ]
-
-
 getVideogameGenres : List Videogame -> List (List String)
 getVideogameGenres games =
     List.map .genres games
 
-
+type alias Computer =
+    { ram : String
+    , model : String
+    , brand : String
+    , screenSize : String
+    }
+myLaptop : Computer
+myLaptop =
+    { ram = "16GB"
+    , model = "MacBook Pro"
+    , brand = "Apple"
+    , screenSize = "14"
+    }
 main : Html msg
 main =
-    text (String.join ", " (onlyStudents users))
+    div []
+        [ div []
+            [ ul []
+                [ li [] [ text ("Ram: " ++ .ram myLaptop) ]
+                , li [] [ text ("Modelo: " ++ .model myLaptop) ]
+                , li [] [ text ("Marca: " ++ .brand myLaptop) ]
+                , li [] [ text ("Pulgadas: " ++ .screenSize myLaptop) ]
+                ]
+            ]
+        ]
+
