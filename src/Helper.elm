13226eb9fa428f0a4145f2f02b3c40ce797f6f9b
Natalia Gonzalez <up230319@alumnos.upa.edu.mxgit config --global user.name up230319-cloudgit pushexit:exitr{gtk+3{rgg{rr{g{{rg{r{gr>
2026-02-15 00:50:47 -0600

    agregar trabajos

--- src/Helper.elm
+++ src/Helper.elm
@@ -1,1 +1,86 @@
-module Helper exposing (..)
+module Main exposing (..)
+
+type alias ProgrammingLanguage =
+    { name : String
+    , releaseYear : Int
+    , currentVersion : String
+    }
+
+languages : List ProgrammingLanguage
+languages =
+    [ { name = "elm", releaseYear = 2012, currentVersion = "0.19.1" }
+    , { name = "javascript", releaseYear = 1995, currentVersion = "ECMAScript 2025" }
+    ]
+
+languageNames : List ProgrammingLanguage -> List String
+languageNames langs =
+    List.map .name langs
+
+
+result : List String
+result =
+    languageNames languages
+
+
+import Html exposing (Html, text)
+import String
+
+
+type alias User =
+    { name : String
+    , uType : String
+    }
+
+
+users : List User
+users =
+    [ { name = "Roberto", uType = "Student" }
+    , { name = "Mitsiu", uType = "Professor" }
+    ]
+
+
+onlyStudents : List User -> List String
+onlyStudents userList =
+    List.map
+        (\user ->
+            case .uType user of
+                "Student" ->
+                    .name user
+
+                _ ->
+                    ""
+        )
+        userList
+
+
+main : Html msg
+main =
+    text (String.join ", " (onlyStudents users))
+
+type alias Videogame =
+    { title : String
+    , releaseYear : Int
+    , available : Bool
+    , downloads : Int
+    , genres : List String
+    }
+videogames : List Videogame
+videogames =
+    [ { title = "Control"
+      , releaseYear = 2019
+      , available = True
+      , downloads = 500000
+      , genres = [ "Action", "Shooter" ]
+      }
+    , { title = "Ocarina of Time"
+      , releaseYear = 1998
+      , available = False
+      , downloads = 1000000
+      , genres = [ "Action", "Adventure" ]
+      }
+    ]
+getVideogameGenres : List Videogame -> List (List String)
+getVideogameGenres games =
+    List.map .genres games
+result =
+    getVideogameGenres videogames
